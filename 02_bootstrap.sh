4e1aedd335d9c15025ceb8bf696af72634d830bb
Łukasz Piątkowski <piontec@gmail.com>
2022-02-15 10:22:32 +0100

    improve bootstrap script

--- 02_bootstrap.sh
+++ 02_bootstrap.sh
@@ -33,7 +33,44 @@ kubectl label cm -n giantswarm --all argocd.argoproj.io/instance-
 kubectl label secret -n giantswarm --all argocd.argoproj.io/instance-
 
 # verify
+for ns in flux-giantswarm flux-system; do
+  for d in helm-controller image-automation-controller image-reflector-controller \
+  kustomize-controller notification-controller source-controller; do
+    kubectl -n $ns wait --for=condition=available --timeout=60s deployment/${d}
+  done
+done
 kubectl -n flux-giantswarm get deploy
 kubectl -n flux-system get deploy
-kubectl -n flux-giantswarm get gitrepo,kustomization
-kubectl -n giantswarm get app | grep -v "deployed"
+echo "*** INFO: all expected Deployments are up"
+
+echo "Checking gitrepos"
+for gr in collection giantswarm-config management-clusters-fleet; do
+  echo -n "Waiting for GitRepo $gr to be ready"
+  while [[ "$(kubectl -n flux-giantswarm get gitrepo $gr --output=jsonpath='{..status.conditions[?(@.type=="Ready")].status}')" != "True" ]]; do
+    sleep 1
+    echo -n "."
+  done
+  echo ""
+done
+echo ""
+echo "*** INFO: all expected GitRepos are up"
+
+echo "Checking kustomizations"
+for gr in flux customer-flux collection; do
+  echo -n "Waiting for Kustomization $gr to be ready"
+  while [[ "$(kubectl -n flux-giantswarm get kustomization $gr --output=jsonpath='{..status.conditions[?(@.type=="Ready")].status}')" != "True" ]]; do
+    sleep 1
+    echo -n "."
+  done
+  echo ""
+done
+echo ""
+echo "\n*** INFO: all expected Kustomizations are up"
+
+kubectl -n giantswarm get app --no-headers | grep -v "deployed"
+if [[ $? -ne 1 ]]; then
+  echo "ERROR: not all Apps in giantswarm namespace are 'deployed'. Check their status."
+  exit 1
+fi
+
+echo "INFO: All done! All checks OK!"
